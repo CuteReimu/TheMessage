3872eae9f0e4ff098ea572168088754a822d2ec8
奇葩の灵梦 <415551921@qq.com>
2022-09-07 02:27:05 +0800

    HumanPlayer相关代码

--- src/main/java/com/fengsheng/HumanPlayer.java
+++ src/main/java/com/fengsheng/HumanPlayer.java
@@ -2,6 +2,9 @@ package com.fengsheng;
 
 import com.fengsheng.card.Card;
 import com.fengsheng.network.ProtoServerChannelHandler;
+import com.fengsheng.phase.DrawPhase;
+import com.fengsheng.phase.MainPhaseIdle;
+import com.fengsheng.phase.SendPhaseStart;
 import com.fengsheng.protos.Common;
 import com.fengsheng.protos.Fengsheng;
 import com.fengsheng.skill.RoleSkillsData;
@@ -12,6 +15,8 @@ import io.netty.buffer.Unpooled;
 import io.netty.channel.Channel;
 import io.netty.util.Timer;
 import org.apache.log4j.Logger;
+
+import java.util.concurrent.TimeUnit;
 
 public class HumanPlayer extends AbstractPlayer {
     private static final Logger log = Logger.getLogger(HumanPlayer.class);
@@ -69,12 +74,31 @@ public class HumanPlayer extends AbstractPlayer {
 
     @Override
     public void notifyDrawPhase() {
-
+        Player player = ((DrawPhase) game.getFsm()).player();
+        int playerId = getAlternativeLocation(player.location());
+        var builder = Fengsheng.notify_phase_toc.newBuilder();
+        builder.setCurrentPlayerId(playerId).setCurrentPhase(Common.phase.Draw_Phase).setWaitingPlayerId(playerId);
+        send(builder.build());
     }
 
     @Override
     public void notifyMainPhase(int waitSecond) {
-
+        Player player = ((MainPhaseIdle) game.getFsm()).player();
+        int playerId = getAlternativeLocation(player.location());
+        var builder = Fengsheng.notify_phase_toc.newBuilder();
+        builder.setCurrentPlayerId(playerId).setCurrentPhase(Common.phase.Main_Phase).setWaitingPlayerId(playerId);
+        builder.setWaitingSecond(waitSecond);
+        if (this == player) {
+            builder.setSeq(seq);
+            final int seq2 = seq;
+            timer = GameExecutor.TimeWheel.newTimeout(timeout -> GameExecutor.post(game, () -> {
+                if (checkSeq(seq2)) {
+                    incrSeq();
+                    game.resolve(new SendPhaseStart(player));
+                }
+            }), waitSecond + 2, TimeUnit.SECONDS).timer();
+        }
+        send(builder.build());
     }
 
     @Override
