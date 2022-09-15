2c3f7dcabebb504850c4cd1dd837729e23f3d98f
奇葩の灵梦 <415551921@qq.com>
2022-09-15 18:31:56 +0800

    修复高并发下加入机器人可能导致房间人数超过满人数的bug

--- src/main/java/com/fengsheng/Game.java
+++ src/main/java/com/fengsheng/Game.java
@@ -25,7 +25,7 @@ public final class Game {
     private static Game newGame;
 
     private final int id;
-    private boolean started;
+    private volatile boolean started;
     private boolean ended;
     private final Player[] players;
     private final Deck deck = new Deck(this);
@@ -71,6 +71,7 @@ public final class Game {
         }
         if (unready == 0) {
             log.info(player + "加入了。已加入" + players.length + "个人，游戏开始。。。");
+            started = true;
             GameExecutor.post(this, this::start);
             newInstance();
         } else {
@@ -83,7 +84,6 @@ public final class Game {
     }
 
     private void start() {
-        started = true;
         Random random = ThreadLocalRandom.current();
         List<Common.color> identities = new ArrayList<>();
         for (int i = 0; i < (players.length - 1) / 2; i++) {
