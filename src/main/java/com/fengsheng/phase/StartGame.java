574fd49c392a4b7778524f037ef627a0b620ecc5
奇葩の灵梦 <415551921@qq.com>
2022-10-07 17:12:26 +0800

    增加游戏开始的日志

--- src/main/java/com/fengsheng/phase/StartGame.java
+++ src/main/java/com/fengsheng/phase/StartGame.java
@@ -2,7 +2,9 @@ package com.fengsheng.phase;
 
 import com.fengsheng.*;
 import com.fengsheng.card.Deck;
+import org.apache.log4j.Logger;
 
+import java.util.Arrays;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
 
@@ -10,10 +12,13 @@ import java.util.concurrent.TimeUnit;
  * 游戏马上开始
  */
 public record StartGame(Game game) implements Fsm {
+    private static final Logger log = Logger.getLogger(StartGame.class);
+
     @Override
     public ResolveResult resolve() {
         Game.GameCache.put(game.getId(), game);
         Player[] players = game.getPlayers();
+        log.info("游戏开始了，场上的角色依次是：" + Arrays.toString(players));
         game.setDeck(new Deck(game, players.length < 5));
         final int whoseTurn = ThreadLocalRandom.current().nextInt(players.length);
         for (int i = 0; i < players.length; i++)
