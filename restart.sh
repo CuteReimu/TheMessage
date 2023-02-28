c5ec2dd89d01c71ac294ff2e83543d78ef2621b6
CuteReimu <415551921@qq.com>
2023-02-28 14:24:44 +0800

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-git pull && ./gradlew build && screen -X -S fengsheng quit && screen -dmS fengsheng java -jar build/libs/fengsheng-1.0.SNAPSHOT-all.jar && screen -ls
+git pull && ./gradlew build && screen -X -S fengsheng quit && screen -dmS fengsheng java -jar build/libs/fengsheng-1.0-SNAPSHOT.jar && screen -ls
