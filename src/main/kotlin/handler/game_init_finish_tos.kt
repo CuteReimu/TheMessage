983ea4b1ac254b3216c6b850343ab800703fb881
CuteReimu <415551921@qq.com>
2023-07-21 14:43:10 +0800

    清理协议

--- src/main/kotlin/handler/game_init_finish_tos.kt
+++ src/main/kotlin/handler/game_init_finish_tos.kt
@@ -2,13 +2,11 @@ package com.fengsheng.handler
 
 import com.fengsheng.HumanPlayer
 import com.fengsheng.phase.WaitForSelectRole
-import com.fengsheng.protos.Fengsheng
 import com.google.protobuf.GeneratedMessageV3
 import org.apache.log4j.Logger
 
 class game_init_finish_tos : ProtoHandler {
     override fun handle(player: HumanPlayer, message: GeneratedMessageV3) {
-        val pb = message as Fengsheng.game_init_finish_tos
         if (player.isLoadingRecord) {
             player.displayRecord()
         } else if (player.isReconnecting) {
