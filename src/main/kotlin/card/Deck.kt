6d80cb630e790cb20ea1dc30f774222c4e67d886
CuteReimu <415551921@qq.com>
2023-08-01 10:41:23 +0800

    两张风云变幻改为带锁定标识

--- src/main/kotlin/card/Deck.kt
+++ src/main/kotlin/card/Deck.kt
@@ -193,8 +193,8 @@ class Deck(private val game: Game) {
             WuDao(98, listOf(color.Black), direction.Right, false),
             WuDao(99, listOf(color.Blue, color.Black), direction.Left, false),
             WuDao(100, listOf(color.Red, color.Black), direction.Right, false),
-            FengYunBianHuan(101, listOf(color.Black), direction.Up, false),
-            FengYunBianHuan(102, listOf(color.Black), direction.Up, false),
+            FengYunBianHuan(101, listOf(color.Black), direction.Up, true),
+            FengYunBianHuan(102, listOf(color.Black), direction.Up, true),
             MiLing(
                 103, listOf(color.Blue, color.Black), direction.Up, true,
                 listOf(color.Blue, color.Black, color.Red)
