011cc48d3a769d4ffd4bdc711daad8e3c90c294e
CuteReimu <415551921@qq.com>
2023-08-09 14:13:52 +0800

    优化代码

--- src/main/kotlin/handler/skill_bo_ai_b_tos.kt
+++ src/main/kotlin/handler/skill_bo_ai_b_tos.kt
@@ -2,13 +2,12 @@ package com.fengsheng.handler
 
 import com.fengsheng.HumanPlayer
 import com.fengsheng.protos.Role
-import com.fengsheng.skill.ActiveSkill
 import com.fengsheng.skill.SkillId
 import org.apache.log4j.Logger
 
 class skill_bo_ai_b_tos : AbstractProtoHandler<Role.skill_bo_ai_b_tos>() {
     override fun handle0(r: HumanPlayer, pb: Role.skill_bo_ai_b_tos) {
-        val skill = r.findSkill(SkillId.BO_AI) as? ActiveSkill
+        val skill = r.findSkill(SkillId.BO_AI)
         if (skill == null) {
             log.error("你没有这个技能")
             r.sendErrorMessage("你没有这个技能")
