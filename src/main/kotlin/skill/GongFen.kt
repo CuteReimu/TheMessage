3df143ec4436e2e5c4a79b8ed10ddc8033ad5fea
奇葩の灵梦 <415551921@qq.com>
2023-08-10 23:31:26 +0800

    修复SP程小蝶发动技能导致一名玩家3黑后，没有进入濒死状态的bug

--- src/main/kotlin/skill/GongFen.kt
+++ src/main/kotlin/skill/GongFen.kt
@@ -2,8 +2,10 @@ package com.fengsheng.skill
 
 import com.fengsheng.*
 import com.fengsheng.card.count
+import com.fengsheng.phase.CheckWin
 import com.fengsheng.phase.FightPhaseIdle
 import com.fengsheng.phase.OnAddMessageCard
+import com.fengsheng.phase.ReceiveOrder
 import com.fengsheng.protos.Common.color.Black
 import com.fengsheng.protos.Role.skill_gong_fen_toc
 import com.fengsheng.protos.Role.skill_gong_fen_tos
@@ -50,7 +52,8 @@ class GongFen : AbstractSkill(), ActiveSkill {
     private data class executeGongFen(
         val fsm: FightPhaseIdle,
         val q: ArrayDeque<Player>,
-        val asMessage: Boolean = false
+        val asMessage: Boolean = false,
+        val receiveOrder: ReceiveOrder = ReceiveOrder(),
     ) : Fsm {
         override fun resolve(): ResolveResult? {
             val target = q.removeFirstOrNull()
@@ -58,7 +61,7 @@ class GongFen : AbstractSkill(), ActiveSkill {
                 if (asMessage) return ResolveResult(
                     OnAddMessageCard(
                         fsm.whoseTurn,
-                        fsm.copy(whoseFightTurn = fsm.inFrontOfWhom)
+                        CheckWin(fsm.whoseTurn, receiveOrder, fsm.copy(whoseFightTurn = fsm.inFrontOfWhom))
                     ), true
                 )
                 return ResolveResult(fsm, true)
@@ -72,6 +75,7 @@ class GongFen : AbstractSkill(), ActiveSkill {
             if (asMessage) {
                 log.info("${card}被置入${target}的情报区")
                 target.messageCards.add(card)
+                receiveOrder.addPlayerIfHasThreeBlack(target)
             } else {
                 log.info("${card}加入${r}的手牌")
                 r.cards.add(card)
