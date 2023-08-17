7e948f0c652760b8d5e3f21249cbcdffc5cded81
CuteReimu <415551921@qq.com>
2023-08-17 19:54:16 +0800

    优化代码

--- src/main/kotlin/phase/NextTurn.kt
+++ src/main/kotlin/phase/NextTurn.kt
@@ -44,11 +44,10 @@ data class NextTurn(val player: Player) : Fsm {
 
     private fun checkDisturberWin(game: Game): Boolean { // 无需判断簒夺者和左右逢源，因为簒夺者、搅局者、左右逢源都要求是自己回合
         val players = game.players.filterNotNull().filter { !it.lose }
-        val disturber = players.find { it.identity == Black && it.secretTask == Disturber } // 搅局者
-        if (player !== disturber) return false
-        if (players.any { it !== disturber && it.alive && it.messageCards.countTrueCard() < 2 }) return false
-        val declaredWinners = arrayOf(disturber)
-        val winner = arrayListOf(disturber)
+        if (player.identity != Black || player.secretTask != Disturber) return false // 不是搅局者
+        if (players.any { it !== player && it.alive && it.messageCards.countTrueCard() < 2 }) return false
+        val declaredWinners = arrayOf(player)
+        val winner = arrayListOf(player)
         if (winner.any { it.findSkill(WEI_SHENG) != null && it.roleFaceUp })
             winner.addAll(players.filter { it.identity == Has_No_Identity })
         val winners = winner.toTypedArray()
