22d9b20f10ffc24025d545ac06922a9046f18a2d
CuteReimu <415551921@qq.com>
2023-10-08 17:41:02 +0800

    教授技能调整

--- src/main/kotlin/skill/RoleCache.kt
+++ src/main/kotlin/skill/RoleCache.kt
@@ -83,6 +83,7 @@ object RoleCache {
     private val forbiddenRoleCache = ArrayList<RoleSkillsData>()
     private val doubleProbabilityRoles = listOf(
         role.si_shi,
+        role.jiao_shou,
         role.za_shua_yi_ren,
         role.bao_da_ting,
         role.jun_ren,
