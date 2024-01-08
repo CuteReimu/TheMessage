a999f11b14b34132ab7f832c61e09d9458e41b96
CuteReimu <415551921@qq.com>
2024-01-08 11:15:13 +0800

    网络增加心跳超时检测

--- src/main/kotlin/network/HttpServerInitializer.kt
+++ src/main/kotlin/network/HttpServerInitializer.kt
@@ -3,11 +3,15 @@ package com.fengsheng.network
 import io.netty.channel.ChannelInitializer
 import io.netty.channel.socket.SocketChannel
 import io.netty.handler.codec.http.HttpServerCodec
+import io.netty.handler.timeout.IdleStateHandler
+import java.util.concurrent.TimeUnit
 
 class HttpServerInitializer : ChannelInitializer<SocketChannel>() {
     override fun initChannel(ch: SocketChannel) {
         val pipeline = ch.pipeline()
+        pipeline.addLast(IdleStateHandler(60, 0, 0, TimeUnit.SECONDS))
         pipeline.addLast("httpServerCodec", HttpServerCodec())
         pipeline.addLast("httpServerHandler", HttpServerChannelHandler())
+        pipeline.addLast(HeartBeatServerHandler())
     }
 }
--- src/main/kotlin/network/WebSocketServerInitializer.kt
+++ src/main/kotlin/network/WebSocketServerInitializer.kt
@@ -5,13 +5,17 @@ import io.netty.channel.socket.SocketChannel
 import io.netty.handler.codec.http.HttpObjectAggregator
 import io.netty.handler.codec.http.HttpServerCodec
 import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler
+import io.netty.handler.timeout.IdleStateHandler
+import java.util.concurrent.TimeUnit
 
 class WebSocketServerInitializer : ChannelInitializer<SocketChannel>() {
     override fun initChannel(ch: SocketChannel) {
         val pipeline = ch.pipeline()
+        pipeline.addLast(IdleStateHandler(60, 0, 0, TimeUnit.SECONDS))
         pipeline.addLast("httpServerCodec", HttpServerCodec())
         pipeline.addLast("http-aggregator", HttpObjectAggregator(65535))
         pipeline.addLast("ws-handler", WebSocketServerProtocolHandler("/ws"))
         pipeline.addLast("webSocketServerHandler", WebSocketServerChannelHandler())
+        pipeline.addLast(HeartBeatServerHandler())
     }
 }
