209111b01d2bc01a2635d5b483ddfb52b5c44523
planetmiku <94736435+planetmiku@users.noreply.github.com>
2024-04-01 02:55:18 +0800

    添加GM命令：给指定玩家获得情报

---
+++ src/main/kotlin/gm/Addmessage.kt
@@ -0,0 +1,71 @@
+package com.fengsheng.gm
+
+import com.fengsheng.Game
+import com.fengsheng.GameExecutor
+import com.fengsheng.card.*
+import com.fengsheng.phase.WaitForSelectRole
+import com.fengsheng.protos.Common.card_type
+import com.fengsheng.protos.Common.card_type.*
+import com.fengsheng.protos.Common.color
+import org.apache.logging.log4j.kotlin.logger
+import java.util.function.Function
+
+class Addmessage : Function<Map<String, String>, Any> {
+    override fun apply(form: Map<String, String>): Any {
+        return try {
+            // player=0&card=0&color=0&count=1
+            val playerId = form["player"]?.toInt() ?: 0
+            val cardTypeNum = form["card"]!!.toInt()
+            val cardType = card_type.forNumber(cardTypeNum)
+            val colorNum = form["colors"]!!.toInt()
+            val color = color.forNumber(colorNum)
+            if (cardType == null || cardType == UNRECOGNIZED) return "{\"error\": \"参数错误\"}"
+            val count = form["count"]
+            val finalCount = count?.toInt()?.coerceIn(1..99) ?: 1
+            val availableCards = Deck.DefaultDeck.filter { it.type == cardType && it.colors.contains(color) }
+            for (g in Game.gameCache.values) {
+                GameExecutor.post(g) {
+                    if (!g.isStarted || g.fsm == null || g.fsm is WaitForSelectRole) return@post
+                    if (playerId < g.players.size && playerId >= 0 && g.players[playerId]!!.alive) {
+                        val cardList = ArrayList<Card>()
+                        for (i in 0 until finalCount) {
+                            val c = availableCards.random()
+                            cardList.add(
+                                when (cardType) {
+                                    Cheng_Qing -> ChengQing(g.deck.getNextId(), c)
+                                    Shi_Tan -> ShiTan(g.deck.getNextId(), c as ShiTan)
+                                    Wei_Bi -> WeiBi(g.deck.getNextId(), c)
+                                    Li_You -> LiYou(g.deck.getNextId(), c)
+                                    Ping_Heng -> PingHeng(g.deck.getNextId(), c)
+                                    Po_Yi -> PoYi(g.deck.getNextId(), c)
+                                    Jie_Huo -> JieHuo(g.deck.getNextId(), c)
+                                    Diao_Bao -> DiaoBao(g.deck.getNextId(), c)
+                                    Wu_Dao -> WuDao(g.deck.getNextId(), c)
+                                    Feng_Yun_Bian_Huan -> FengYunBianHuan(g.deck.getNextId(), c)
+                                    Mi_Ling -> MiLing(g.deck.getNextId(), c as MiLing)
+                                    Diao_Hu_Li_Shan -> DiaoHuLiShan(g.deck.getNextId(), c)
+                                    Yu_Qin_Gu_Zong -> YuQinGuZong(g.deck.getNextId(), c)
+                                    else -> throw IllegalStateException("Unexpected value: $cardType")
+                                }
+                            )
+                        }
+                        val p = g.players[playerId]!!
+                        p.messageCards.addAll(cardList)
+                        logger.info("由于GM命令，${p}获得了${cardList.joinToString()}张情报")
+                        for (player in g.players) {
+                            if (player!!.location == playerId)
+                                player.notifyAddHandCard(playerId, 0, cardList)
+                            else
+                                player.notifyAddHandCard(playerId, cardList.size)
+                        }
+                    }
+                }
+            }
+            "{\"msg\": \"成功\"}"
+        } catch (e: NumberFormatException) {
+            "{\"error\": \"参数错误\"}"
+        } catch (e: NullPointerException) {
+            "{\"error\": \"参数错误\"}"
+        }
+    }
+}
