efc5626d0af1066cee8d89cc4f82d1e897c07807
zc-meng <zc_meng@tju.edu.cn>
2024-08-22 20:09:37 +0800

    郑文先面朝上时，手里有破译就出

--- src/main/kotlin/card/PoYi.kt
+++ src/main/kotlin/card/PoYi.kt
@@ -7,6 +7,7 @@ import com.fengsheng.phase.SendPhaseIdle
 import com.fengsheng.protos.Common.card_type.Po_Yi
 import com.fengsheng.protos.Common.color
 import com.fengsheng.protos.Common.direction
+import com.fengsheng.protos.Common.role.*
 import com.fengsheng.protos.Fengsheng.po_yi_show_tos
 import com.fengsheng.protos.poYiShowToc
 import com.fengsheng.protos.poYiShowTos
@@ -136,7 +137,10 @@ class PoYi : Card {
             val player = e.inFrontOfWhom
             card.type == Po_Yi || return false // 机器人不要把别的牌当破译用
             !player.cannotPlayCard(Po_Yi) || return false
-            !e.isMessageCardFaceUp && e.messageCard.isBlack() || return false
+            // 郑文先面朝上时，手里有破译就出
+            player.role == zheng_wen_xian && player.roleFaceUp ||
+                !e.isMessageCardFaceUp && e.messageCard.isBlack() ||
+                return false
             GameExecutor.post(player.game!!, {
                 convertCardSkill?.onConvert(player)
                 card.asCard(Po_Yi).execute(player.game!!, player)
