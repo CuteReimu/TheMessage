4a5aa15133c6f5680d1b3ad615097cd727247c66
奇葩の灵梦 <cutereimu@vip.qq.com>
2024-11-11 16:33:55 +0800

    建议等待其他玩家一起游戏

--- src/main/kotlin/handler/AddRobotTos.kt
+++ src/main/kotlin/handler/AddRobotTos.kt
@@ -25,9 +25,17 @@ class AddRobotTos : AbstractProtoHandler<Fengsheng.add_robot_tos>() {
 //                }
 //            }
             val humanCount = r.game!!.players.count { it is HumanPlayer }
-            if (humanCount <= 1 && emptyPosition == 1 && Statistics.getEnergy(r.playerName) <= 0) {
-                r.sendErrorMessage("""你的精力不足，不能进行人机局。你可以在群里输入"精力系统"了解。""")
-                return
+            if (humanCount <= 1 && emptyPosition == 1) {
+                if (Statistics.getEnergy(r.playerName) <= 0) {
+                    r.sendErrorMessage("""你的精力不足，不能进行人机局。你可以在群里输入"精力系统"了解。""")
+                    return
+                }
+                if (Game.gameCache.count { (_, v) -> // 未开始或有空位的房间（含本房间）大于1，则禁止开局
+                        !v.isStarted || v.players.any { it !is HumanPlayer }
+                    } > 1) {
+                    r.sendErrorMessage("""有其他玩家正在进行游戏，请等待他们一起吧！""")
+                    return
+                }
             }
         }
         val robotPlayer = RobotPlayer()
