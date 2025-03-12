1dbd0262817699f4a9e4470c44642e56fe06550b
奇葩の灵梦 <cutereimu@vip.qq.com>
2025-03-12 11:35:18 +0800

    调整高分玩家神秘人获胜计算规则

--- src/main/kotlin/ScoreFactory.kt
+++ src/main/kotlin/ScoreFactory.kt
@@ -113,9 +113,9 @@ object ScoreFactory : Logging {
             }
             if (identity == Has_No_Identity) score /= winners.count { it.identity == Has_No_Identity }.coerceAtLeast(1)
             score *= 1 + delta / 100.0 / when {
-                originIdentity != Black -> 1
-                players.size <= 6 -> 2
-                else -> 3
+                originIdentity != Black -> 1.0
+                players.size <= 6 -> 2.0 / winners.size
+                else -> 3.0 / winners.size
             }
         } else {
             score = Score(if (players.size <= 6) -7.0 else -12.0)
